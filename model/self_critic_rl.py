b1d68d6f2dc4115a48997dda15164a68dbd29475
paulko <seongwoon.ko@gmail.com>
2018-08-30 20:20:41 +0900

    wip

--- model/self_critic_rl.py
+++ model/self_critic_rl.py
@@ -21,7 +21,8 @@ def self_critic_rl(options, ce_loss, start_pos_list, end_pos_list):
         greedy_end_pos_list = tf.placeholder(tf.float32, shape=shape, name='greedy_start_pos_list')
         reward = tf.placeholder(tf.float32, shape=shape[0:2], name='reward')
 
-        sampled_start_pos_log_p = tf.multiply(tf.multiply(sampled_start_pos_list, tf.log(start_pos_list)), reward)
+        temp = tf.multiply(sampled_start_pos_list, tf.log(start_pos_list))
+        sampled_start_pos_log_p = tf.multiply(temp, reward)
         sampled_start_adv = tf.reduce_mean(tf.reduce_sum(-1 * sampled_start_pos_log_p, axis=1))
         sampled_end_pos_log_p = sampled_end_pos_list * tf.log(end_pos_list) * reward
         sampled_end_adv = tf.reduce_mean(tf.reduce_sum(-1 * sampled_end_pos_log_p, axis=1))
