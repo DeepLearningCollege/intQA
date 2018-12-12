795e5f4b29c3d49a4d33cae572e3b3cbb35adf36
parksurk <parksurk@gmail.com>
2018-12-13 03:28:20 +0900

    flask web app 소스 수정

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from train.evaluator import Evaluator
 from flags import get_options_from_flags
 
 def del_all_flags(FLAGS):
-    flags_dict = FLAGS._flags()
+    flags_dict = FLAGS.__flags()
     keys_list = [keys for keys in flags_dict]
     for keys in keys_list:
         FLAGS.__delattr__(keys)
